eeeaac931c98910dc320cacc321fb5941ec9249f
d0natien <contact@donatien.net>
2020-10-02 15:46:23 +0200

    touche finale

--- bouton_poussoir.js
+++ bouton_poussoir.js
@@ -15,18 +15,22 @@ let switchOn = true;
 function switchOnOff(){
   
   if(switchOn){
-  bouton.style.color = '#89ed0e';
+  bouton.style.position = "absolute";
+  bouton.style.top = "125px"; /*simulation d'enfoncement*/
+  bouton.style.color = '#b3ff00';
   bouton.style.background = "#ffffff";
-  bouton.style.textShadow = "0px 0px 10px #c8ff85, 0px 0px 10px #c8ff85, 0px 0px 10px #c8ff85";
+  bouton.style.textShadow = "0px 0px 10px #c8ff85, 0px 0px 10px #c8ff85, 0px 0px 10px #c8ff85"; /*superposition pour un effet lumineux*/
   bouton.style.boxShadow = "inset 0px 1px 0px #aaa, 0px 1px 0px 1px #156785";
   
   switchOn = false;
 
 }else{
+  bouton.style.position = "absolute";
+  bouton.style.top = "120px";
   bouton.style.border = "1px solid black";
   bouton.style.color = "#808080";
-  bouton.style.boxShadow = "inset 0px 1px 0px #aaa, 0px 7px 0px 1px #156785";
   bouton.style.boxShadow = "inset 0px 1px 0px #aaa, 0px 7px 0px 1px #156785, 8px 7px 7px grey";
+  bouton.style.textShadow = "none";
   
   switchOn = true;
 }
